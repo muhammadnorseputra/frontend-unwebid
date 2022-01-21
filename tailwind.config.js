f975b9843614f7064de1fa215556a9d7498c2095
Purtabungsu <muhammadnorseputra@gmail.com>
2022-01-21 10:10:53 +0800

    up home

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
   },
   plugins: [
   	require('tailwindcss-textshadow'),
-  	require('tailwind-scrollbar')
+  	require('tailwind-scrollbar'),
+    require('autoprefixer')
   ]
 }
